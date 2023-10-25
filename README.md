d4b41acd9bed62574181bd8efd9e67564d5308c1
NevesLaura-NP <neveslaura1997@gmail.com>
2023-10-24 20:51:56 -0400

    yml

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# bank-TLAB
-This is a TKH activity
+# README
+
+Credit to the [Data Science Working Group](http://datascience.codeforsanfrancisco.org) for this template. To complete this project, delete all template text (save for the headers) and fill in your own information.
+
+## Project Intro/Objective
+The purpose of this project is to continue a pipeline in Python to analyse stock data. We aim to integrate it with daily news headline data to measure how reliably sentiment analysis can predict stock prices. Due to stocks being heavily influenced by investors sentiments upon economy and the market, a sentiment analysis could offer new variables of prediction for updated stock prices leading to greater opportunities for profit.
+
+### Methods Used
+* Predictive Modeling
+
+### Technologies
+* Python
+* Jupyter
+
+## Project Description
+Our current sample dataset contains 9 weeks of Amazon stock prices. We currently are able to create classes that can measure the mean, median and standart deviation of the stock prices to predict its ups and downs in prices. Once those statistics were analyzed for the stock we can trace the week in which the prices were most affected and in the future we can analyse the price difference according to the news headlines.
---
+++ code/StockMetrics.py
@@ -0,0 +1,66 @@
+
+import statistics as stats
+
+from StockData import StockData
+
+
+class StockMetrics(StockData):
+    def __init__(self, path):
+        # call the parent method's constructor
+        super(StockMetrics, self).__init__(path)
+
+        # now that we've ran self.load(), we can interact with "self.data" as a
+        # list of lists
+        self.load()
+    
+
+    def valid_float(self, val):
+        
+        try:
+            float_val = float(val)
+            return True
+        except ValueError:
+            return False
+        
+        
+    def average01(self):
+        
+        averages = []
+
+        for row in self.data:
+            new_vals = [float(price) for price in row if self.valid_float(price)]
+            if new_vals:
+                row_avg = round(stats.mean(new_vals), 3)
+                averages.append(row_avg)
+
+        return averages
+
+
+    def median02(self):
+        """pt2
+        """
+        median = []
+
+        for row in self.data:
+            new_vals = [float(price) for price in row if self.valid_float(price)]
+            if new_vals:
+                row_med = stats.median(new_vals)
+                median.append(row_med)
+
+        return median
+
+    def stddev03(self):
+        """pt3
+        """
+        stdev = []
+        
+        for row in self.data:
+            new_vals = [float(price) for price in row if self.valid_float(price)]
+            if new_vals:
+                row_stdev = round(stats.stdev(new_vals), 3)
+                stdev.append(row_stdev)
+                
+        return stdev
+                
+
+
